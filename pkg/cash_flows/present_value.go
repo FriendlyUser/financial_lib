61de76ba00b3c31c19f20ce1bec5100a40a3694b
David Li <davidli012345@gmail.com>
2021-04-24 17:31:13 -0700

    updating documentation

--- pkg/cash_flows/present_value.go
+++ pkg/cash_flows/present_value.go
@@ -9,18 +9,22 @@ type CashFlowCalculator struct {
 	TimePeriods  []float64
 }
 
-/* Formula for present value (PV) for a future payment is determined by
- * PV = FV / (1 + R) ^ N
- * PV is the desired present value
- * FV is the future value that we want to discount
- * N is the number of periods between present and future value
- */
+// Formula for present value (PV) for a future payment is determined by
+//
+// PV = FV / (1 + R) ^ N
+//
+// PV is the desired present value
+//
+// FV is the future value that we want to discount
+//
+// N is the number of periods between present and future value
 func (cfc *CashFlowCalculator) CalcPresentValue(futureValue float64, timePeriod float64) float64 {
 	pValue := futureValue / math.Pow(1+cfc.Rate, timePeriod)
 	return pValue
 }
 
 // gets the present value of all cash payments
+
 // see cash_flows.CalcPresentValue
 func (cfc *CashFlowCalculator) PresentValue() float64 {
 	total := 0.0
--- pkg/interest/compound_interest.go
+++ pkg/interest/compound_interest.go
@@ -7,25 +7,33 @@ type CompoundRateCalculator struct {
 	Rate float64
 }
 
-/*
- * Formula for discrete	compound interest is
- * V = P(1+R)^N
- * where P is the present value
- * R is the rate
- * N is the number of periods
- */
+//
+// Formula for discrete	compound interest is
+//
+// V = P(1+R)^N
+//
+// where P is the present value
+//
+// R is the rate
+//
+// N is the number of periods
 func (r *CompoundRateCalculator) MultiplePeriod(value float64, numPeriods float64) float64 {
 	return value * math.Pow(1+r.Rate, numPeriods)
 }
 
-/*
- * The formula for the calculation of continuous interest rate compounding is
- * V = Pe^(RN)
- * V is desired future value
- * P is the present value
- * R is the interest rate during this period
- * N is the number of periods
- */
+//
+// The formula for the calculation of continuous interest rate compounding is:
+//
+// V = Pe^(RN)
+//
+// V is desired future value
+//
+// P is the present value
+//
+// R is the interest rate during this period
+//
+// N is the number of periods
+//
 func (r *CompoundRateCalculator) ContinuousCompounding(value float64, numPeriods float64) float64 {
 	return value * math.Exp(r.Rate*numPeriods)
 }
